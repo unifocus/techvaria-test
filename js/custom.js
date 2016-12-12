11b26a423fdb7a66e3a29300418bcd19e520ea2b
unifocus <minin@unifocusmedia.com>
2016-12-12 13:24:41 +0530

    sie

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+	jQuery(document).ready(function(){
+    $(".dropdown").hover(
+        function() { $('.dropdown-menu', this).stop().fadeIn("fast");
+        },
+        function() { $('.dropdown-menu', this).stop().fadeOut("fast");
+    });
+});
